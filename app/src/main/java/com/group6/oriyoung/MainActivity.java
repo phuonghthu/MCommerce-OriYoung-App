d3cee7454d3fe4fc711e100db73f34bb13637b6d
phtEC <phuonghuynh.ec@gmail.com>
2024-03-24 22:58:59 +0700

    Init Project

---
+++ app/src/main/java/com/group6/oriyoung/MainActivity.java
@@ -0,0 +1,24 @@
+package com.group6.oriyoung;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group6.oriyoung.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
