b3aefe7e3981a66784c465afc10cffbcd1bbe482
phtEC <phuonghuynh.ec@gmail.com>
2024-03-28 11:15:35 +0700

    onboarding screen

---
+++ app/src/main/java/com/group6/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.group6.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    ArrayList<Fragment> fragmentList;
+    FragmentManager fragmentManager;
+    Lifecycle lifecycle;
+
+    public ViewPagerAdapter(ArrayList<Fragment> fragmentList, FragmentManager supportFragmentManager, Lifecycle lifecycle) {
+        super(supportFragmentManager, lifecycle);
+        this.fragmentList = fragmentList;
+        this.fragmentManager = supportFragmentManager;
+        this.lifecycle = lifecycle;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
