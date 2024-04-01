f833a206ebc2cba4084153fbd79d7fb2e3a8e527
phtEC <phuonghuynh.ec@gmail.com>
2024-04-01 23:28:27 +0700

    home activity

---
+++ app/src/main/java/com/group6/Fragments/WishlistFragment.java
@@ -0,0 +1,27 @@
+package com.group6.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group6.oriyoung.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WishlistFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WishlistFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wishlist, container, false);
+    }
+}
