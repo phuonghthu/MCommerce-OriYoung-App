af6e01c7d566f23c94e630e00d45ee08aa64dede
phtEC <phuonghuynh.ec@gmail.com>
2024-04-01 18:45:54 +0700

    bottom_nav

---
+++ app/src/main/java/com/group6/oriyoung/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.group6.oriyoung;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group6.oriyoung.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
