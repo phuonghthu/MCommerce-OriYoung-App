e8b53d8a773c2a40862f076139730cb0ba393ecd
tueminh1511 <tueminhst2018@gmail.com>
2024-04-14 21:30:40 +0700

    Set gridview for category

--- app/src/main/java/com/group6/oriyoung/MenuSearch.java
+++ app/src/main/java/com/group6/oriyoung/MenuSearch.java
@@ -1,24 +1,41 @@
 package com.group6.oriyoung;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.group6.adapters.CategoryNameAdapter;
+import com.group6.models.Category;
+import com.group6.oriyoung.databinding.ActivityMenuSearchBinding;
+import java.util.ArrayList;
 
 public class MenuSearch extends AppCompatActivity {
+    ActivityMenuSearchBinding binding;
+    CategoryNameAdapter CategoryNameAdapter;
+
+    ArrayList<Category> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu_search);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMenuSearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize list of categories
+        categories = new ArrayList<>();
+        categories.add(new Category(0, "Tẩy trang"));
+        categories.add(new Category(0, "Sữa rửa mặt"));
+        categories.add(new Category(0, "Serum"));
+        categories.add(new Category(0, "Kem dưỡng"));
+        categories.add(new Category(0, "Chống nắng"));
+        categories.add(new Category(0, "Mặt nạ"));
+        categories.add(new Category(0, "Xịt khoáng"));
+        categories.add(new Category(0, "Dưỡng mắt"));
+        categories.add(new Category(0, "Dưỡng môi"));
+        // Add other categories here...
+
+        // Initialize Adapter and set up GridView
+        CategoryNameAdapter = new CategoryNameAdapter(this, categories);
+        GridView gridView = findViewById(R.id.category_grid_view);
+        gridView.setAdapter(CategoryNameAdapter);
     }
 }
