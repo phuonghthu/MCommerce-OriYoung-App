946760ba35abc2df6a4c06c1fde38cc23b0624bc
phtEC <phuonghuynh.ec@gmail.com>
2024-04-15 01:44:17 +0700

    wishlist UI

---
+++ app/src/main/java/com/group6/adapters/FavoriteAdapter.java
@@ -0,0 +1,66 @@
+package com.group6.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group6.models.Product;
+import com.group6.oriyoung.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.FavoriteViewholder>{
+
+    Context context;
+    ArrayList<Product> favProduct;
+
+    public FavoriteAdapter(Context context, ArrayList<Product> favProduct) {
+        this.context = context;
+        this.favProduct = favProduct;
+    }
+
+    @NonNull
+    @Override
+    public FavoriteViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_product, parent, false);
+        return new FavoriteAdapter.FavoriteViewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteViewholder holder, int position) {
+        Product p = favProduct.get(position);
+
+        holder.imvProductThumb.setImageResource(p.getProductImage());
+        holder.txtName.setText(p.getProductName());
+        holder.txtPrice.setText(String.valueOf(Math.round(p.getProductPrice())) + " VNĐ");
+    }
+
+    @Override
+    public int getItemCount() {
+        return favProduct.size();
+    }
+
+    public class FavoriteViewholder extends RecyclerView.ViewHolder{
+        ImageView imvProductThumb, imvAddToFav;
+        TextView txtName, txtPrice, txtRatingValue, btnAddToCart;
+
+
+        public FavoriteViewholder(@NonNull View itemView) {
+            super(itemView);
+            imvProductThumb= itemView.findViewById(R.id.imvProductThumb);
+            txtName= itemView.findViewById(R.id.txtName);
+            txtPrice= itemView.findViewById(R.id.txtPrice);
+            txtRatingValue = itemView.findViewById(R.id.txtRatingValue);
+            btnAddToCart = itemView.findViewById(R.id.btnAddToCart);
+            imvAddToFav = itemView.findViewById(R.id.imvAddToFav);
+        }
+    }
+}
