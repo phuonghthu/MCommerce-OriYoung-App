87c93478cbea192d4d267fa42ba7753794317edf
juky54 <huonggiangcao23@gmail.com>
2024-04-15 13:19:42 +0700

    product detail UI ongoing

--- app/src/main/java/com/group6/oriyoung/ProductDetail.java
+++ app/src/main/java/com/group6/oriyoung/ProductDetail.java
@@ -1,6 +1,8 @@
 package com.group6.oriyoung;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.group6.oriyoung.databinding.ActivityProductDetailBinding;
+
 public class ProductDetail extends AppCompatActivity {
+
+    ActivityProductDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_product_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityProductDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(ProductDetail.this, )
+            }
+        });
+        binding.GroupButton.btnBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductDetail.this, CartActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
