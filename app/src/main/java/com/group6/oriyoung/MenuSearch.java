9f9497c28455170f076a4dd4b58313c8f721c85a
tueminh1511 <tueminhst2018@gmail.com>
2024-04-15 09:47:12 +0700

    Set listview appear when user click in search bar

--- app/src/main/java/com/group6/oriyoung/MenuSearch.java
+++ app/src/main/java/com/group6/oriyoung/MenuSearch.java
@@ -1,15 +1,18 @@
 package com.group6.oriyoung;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.group6.adapters.CategoryNameAdapter;
 import com.group6.adapters.SearchListProductAdapter;
 import com.group6.models.Category;
 import com.group6.models.Product;
 import com.group6.oriyoung.databinding.ActivityMenuSearchBinding;
+
 import java.util.ArrayList;
 
 public class MenuSearch extends AppCompatActivity {
@@ -17,7 +20,6 @@ public class MenuSearch extends AppCompatActivity {
     CategoryNameAdapter categoryNameAdapter;
     SearchListProductAdapter productAdapter;
     ListView listView;
-
     ArrayList<Product> products;
     ArrayList<Category> categories;
 
@@ -54,10 +56,29 @@ public class MenuSearch extends AppCompatActivity {
         products = new ArrayList<>();
         products.add(new Product(0, 0,"Nước Tẩy Trang L'Oreal Tươi Mát Cho Da Dầu, Hỗn Hợp 400ml\n" +
                 "    Micellar Water 3-in-1 Refreshing Even For Sensitive Skin", 100000, 0, null, R.drawable.product_place_holder, false, false, 0, 0,null));
+        products.add(new Product(0, 0,"Bông Tẩy Trang L'Oreal Tươi Mát Cho Da Dầu, Hỗn Hợp 400ml\n" +
+                "    Micellar Water 3-in-1 Refreshing Even For Sensitive Skin", 100000, 0, null, R.drawable.product_place_holder, false, false, 0, 0,null));
+        products.add(new Product(0, 0,"Con Tẩy Trang L'Oreal Tươi Mát Cho Da Dầu, Hỗn Hợp 400ml\n" +
+                "    Micellar Water 3-in-1 Refreshing Even For Sensitive Skin", 100000, 0, null, R.drawable.product_place_holder, false, false, 0, 0,null));
         // Add products here...
 
         // Initialize Adapter and set up ListView for products
         productAdapter = new SearchListProductAdapter(this, products);
         listView.setAdapter(productAdapter);
+
+        // Hide ListView initially
+        listView.setVisibility(View.GONE);
+
+        // Show ListView when search bar gains focus
+        binding.searchBar.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    listView.setVisibility(View.VISIBLE);
+                } else {
+                    listView.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
