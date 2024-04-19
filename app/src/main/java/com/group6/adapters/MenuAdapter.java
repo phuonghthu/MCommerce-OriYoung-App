1027804caa1ec9f3ab496033489fdba6ae303e0e
phtEC <phuonghuynh.ec@gmail.com>
2024-04-20 01:35:39 +0700

    intent categoryfragment

--- app/src/main/java/com/group6/adapters/MenuAdapter.java
+++ app/src/main/java/com/group6/adapters/MenuAdapter.java
@@ -1,6 +1,7 @@
 package com.group6.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.group6.models.Category;
+import com.group6.oriyoung.ProductCatalog;
 import com.group6.oriyoung.R;
 
 import java.util.ArrayList;
@@ -43,7 +45,25 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
             Glide.with(context).load(drawableResoucreID).into(holder.imvCateThumb);
         }
         holder.txtCateName.setText(categories.get(position).getCategoryName());
-
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Category clickedCategory = categories.get(position);
+                if (clickedCategory.getCategoryID() == 7) {
+                    // Hiển thị tất cả sản phẩm
+                    Intent intent = new Intent(context, ProductCatalog.class);
+                    intent.putExtra("categoryID", 7);
+                    intent.putExtra("categoryName", "Tất cả sản phẩm");
+                    context.startActivity(intent);
+                } else {
+                    // Hiển thị danh sách sản phẩm trong danh mục đã chọn
+                    Intent intent = new Intent(context, ProductCatalog.class);
+                    intent.putExtra("categoryID", clickedCategory.getCategoryID());
+                    intent.putExtra("categoryName", clickedCategory.getCategoryName());
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
